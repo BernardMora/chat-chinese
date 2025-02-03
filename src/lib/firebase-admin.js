a5ea1f89773c6ba6d62a67d7d087a338e925a003
bernardmora <morales.bernardo@uabc.edu.mx>
2025-02-03 10:36:37 -0800

    changed way getting env variables for production

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -1,11 +1,19 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "@/serviceAccountKey.json";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Now parse the JSON string into an object
+let firebaseServiceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+firebaseServiceAccount["private_key"] = firebaseServiceAccount[
+  "private_key"
+].replaceAll("{code}", "\n");
 
 // Initialize the Firebase Admin SDK
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: cert(firebaseServiceAccount),
   });
 }
 
